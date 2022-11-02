9b067229042851954307697c3634d74966a0246b
Mhayk Whandson <hi@mhayk.com>
2022-11-02 11:32:32 +0000

    feat: customize the status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import { NativeBaseProvider, Text, Center } from 'native-base'
+import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from './src/components/Loading';
 
@@ -12,9 +12,13 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      {
-        fontsLoaded ? <SignIn /> : <Loading />
-      }
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
