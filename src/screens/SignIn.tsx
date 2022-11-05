cac2011edd777dd0355abe6660101f8b545d7787
Mhayk Whandson <hi@mhayk.com>
2022-11-05 15:55:08 +0000

    feat: create signin page

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,25 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
 
 import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button";
 
 export function SignIn() {
     return (
-        <Center flex={1} bgColor="gray.900" alignItems="center" justifyContent="center">
+        <Center flex={1} bgColor="gray.900" alignItems="center" justifyContent="center" p={7}>
             <Logo width={212} height={40} />
+
+            <Button
+                type="SECONDARY"
+                title="SIGN IN WITH GOOGLE"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                mt={12}
+            />
+
+            <Text color="white" textAlign="center" mt={4}>
+                We do not use any information other than {'\n'} your email to create your account.
+            </Text>
+
         </Center>
     )
 }
