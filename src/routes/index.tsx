85d056eb5529f2b9b368e545ef060aa5537bb444
Mhayk Whandson <hi@mhayk.com>
2022-11-12 22:04:39 +0000

    feat: show spinner and redirect to AppRoutes if the user is authenticated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native'
+
+import { useAuth } from '../hooks/useAuth'
+
 import { AppRoutes } from './app.routes'
+import { SignIn } from '../screens/SignIn'
+import { Box } from 'native-base'
 
 export function Routes() {
+    const { user } = useAuth()
     return (
-        <NavigationContainer>
-            <AppRoutes />
-        </NavigationContainer>
+        <Box flex={1} bg="gray.900"> // avoid glitter
+            <NavigationContainer>
+                {user.name ? <AppRoutes /> : <SignIn />}
+            </NavigationContainer>
+        </Box>
     )
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -6,7 +6,7 @@ import Logo from '../assets/logo.svg'
 import { Button } from "../components/Button";
 
 export function SignIn() {
-    const { signIn } = useAuth()
+    const { signIn, isUserLoading } = useAuth()
     return (
         <Center flex={1} bgColor="gray.900" alignItems="center" justifyContent="center" p={7}>
             <Logo width={212} height={40} />
@@ -17,6 +17,8 @@ export function SignIn() {
                 leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
                 mt={12}
                 onPress={signIn}
+                isLoading={isUserLoading}
+                _loading={{ _spinner: { color: 'white' } }}
             />
 
             <Text color="white" textAlign="center" mt={4}>
