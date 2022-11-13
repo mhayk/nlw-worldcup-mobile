d9f53a57600c699af2f5d114c9cebb60a6088e13
Mhayk Whandson <hi@mhayk.com>
2022-11-13 15:08:27 +0000

    feat: integrate polls screen with the api backend to list all polls

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,12 +1,67 @@
-import { VStack, Icon } from "native-base";
+import { useEffect, useState } from 'react'
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from '@expo/vector-icons'
 import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { PoolCard } from '../components/PoolCard';
+import { Loading } from '../components/Loading';
+
+import { api } from "../services/api";
+import { EmptyPollList } from '../components/EmptyPollList';
+
+interface Participant {
+    id: string;
+    user: {
+        name: string;
+        avatarUrl: string;
+    };
+};
+
+interface Pool {
+    id: string;
+    code: string;
+    title: string;
+    ownerId: string;
+    createdAt: string;
+    owner: {
+        name: string;
+    },
+    participants: Participant[];
+    _count: {
+        participants: number;
+    }
+}
 
 export function Pools() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [polls, setPolls] = useState<Pool[]>([])
+
+    const toast = useToast()
     const { navigate } = useNavigation()
+
+    async function fetchPools() {
+        try {
+            setIsLoading(true)
+            const response = await api.get('/polls')
+            setPolls(response.data.polls)
+        } catch (error) {
+            console.log(error)
+            toast.show({
+                title: 'I could not load the polls',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchPools()
+    }, [])
 
     return (
         <VStack flex={1} bgColor="gray.900">
@@ -17,6 +72,7 @@ export function Pools() {
                 borderBottomWidth={1}
                 borderBottomColor="gray.600"
                 pb={4}
+                mb={4}
             >
                 <Button
                     leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
@@ -25,6 +81,17 @@ export function Pools() {
                 />
 
             </VStack>
+
+            {isLoading ? <Loading /> :
+                <FlatList
+                    data={polls}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <PoolCard data={item} />}
+                    ListEmptyComponent={() => <EmptyPollList />}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ pb: 10 }}
+                    px={5}
+                />}
         </VStack>
     )
 }
