9ac1165ce5ca1bcb4668ca41e27a362823b0daa2
Mhayk Whandson <hi@mhayk.com>
2022-11-13 13:53:43 +0000

    feat: integrate the poll creation with the backend

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,10 +1,55 @@
-import { Heading, VStack, Text } from "native-base";
+import { Heading, VStack, Text, useToast } from "native-base";
+import { useState } from "react";
+import { Alert } from "react-native";
 import Logo from '../assets/logo.svg'
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 
+import { api } from "../services/api";
+
 export function New() {
+    const [title, setTitle] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const toast = useToast()
+
+    async function handlePollCreate() {
+        if (!title.trim()) {
+            // Alert.alert('New Poll', 'Enter a poll name!')
+            return toast.show({
+                title: 'Enter a valid poll name, please!',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
+
+        try {
+            setIsLoading(true)
+
+            await api.post('/polls', { title })
+
+            toast.show({
+                title: 'Poll has been created successfully!',
+                placement: 'top',
+                bgColor: 'green.500'
+            })
+
+            setTitle('')
+        } catch (error) {
+            console.log(error)
+
+            toast.show({
+                title: 'It was not possible to create a new poll',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+            throw error
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
         <VStack flex={1} bgColor="gray.900">
             <Header title="Create a new pool" />
@@ -13,17 +58,21 @@ export function New() {
                 <Logo />
 
                 <Heading fontFamily="heading" color="white" fontSize="xl" my={8} textAlign="center">
-                    Create your own cup pool and{'\n'}
+                    Create your own cup poll and{'\n'}
                     share it with friends!
                 </Heading>
 
                 <Input
                     mb={2}
-                    placeholder="What is the name of your pool ?"
+                    placeholder="What is the name of your poll ?"
+                    onChangeText={setTitle}
+                    value={title}
                 />
 
                 <Button
-                    title="CREATE MY POOL"
+                    title="CREATE MY POLL"
+                    onPress={handlePollCreate}
+                    isLoading={isLoading}
                 />
 
                 <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
