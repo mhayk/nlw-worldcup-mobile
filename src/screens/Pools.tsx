3fe4f6fa76455544a4e27a92a31fb434ee3c8057
Mhayk Whandson <hi@mhayk.com>
2022-11-13 15:13:30 +0000

    feat: invoke useFocusEffect to reload all polls when the screen has focus in My Polls screen

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from '@expo/vector-icons'
 import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect } from '@react-navigation/native'
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
@@ -59,9 +60,9 @@ export function Pools() {
         }
     }
 
-    useEffect(() => {
+    useFocusEffect(useCallback(() => {
         fetchPools()
-    }, [])
+    }, []))
 
     return (
         <VStack flex={1} bgColor="gray.900">
